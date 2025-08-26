7dbe0fa5cf1640536b653bf78ebdf081deff4879
MasudAlamMolla99 <masudalammolla99@gmail.com>
2025-08-26 22:26:15 +0530

    Add Schema validation

--- src/app.js
+++ src/app.js
@@ -7,14 +7,34 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   // console.log(req.body);
-  // const newUser = new User();
-  // try {
-  //   await newUser.save();
-  //   res.send("user created successfully");
-  // } catch (err) {
-  //   res.send("Error " + err.message);
-  // }
+  const newUser = new User(req.body);
+  try {
+    await newUser.save();
+    res.send("user created successfully");
+  } catch (err) {
+    res.send("Error " + err.message);
+  }
 });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something error");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(401).send("Something error");
+  }
+});
+
+app.delete("/user", (req, res) => {});
 
 connectDb()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,66 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      maxLength: 12,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      max: 50,
+    },
+    gender: {
+      type: String,
+      required: true,
+
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "Add about yourself",
+      validate(value) {
+        if (!/^[a-zA-Z0-9\s]+$/.test(value)) {
+          throw new Error("About section must not contain special characters");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
